7a634086987024e861f43dfb43b37b55059f11bb
Shuvo-saha01 <onlyshuvosaha11@gmail.com>
2024-12-16 00:52:53 +0530

    database name fixed

--- Utils/db.js
+++ Utils/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
         const instance = await mongoose.connect(process.env.MONGO_URL)
         console.log(`MongoDB Connected: ${instance.connection.host}`);
-        
+        console.log("database name", mongoose.connection.name)
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
